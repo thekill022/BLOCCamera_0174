6182604345149b0517c4fe10195030b1e6d47c3b
thekill022 <gibfathoni@gmail.com>
2026-04-17 09:55:20 +0700

    membuat class untuk event camera pada bloc

---
+++ lib/bloc/camera_event.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+sealed class CameraEvent{}
+
+final class IntializeCamera extends CameraEvent {}
+final class SwitchCamera extends CameraEvent {}
+final class ToggleFlash extends CameraEvent {}
+
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
+final class TapToFocus extends CameraEvent {
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
+final class PickImageFromGallery extends CameraEvent {}
+
+final class OpenCameraAndCapture extends CameraEvent {
+  final BuildContext context;
+  OpenCameraAndCapture(this.context);
+}
+final class DeleteImage extends CameraEvent{}
+final class ClearSnackbar extends CameraEvent{}
+final class RequestPermission extends CameraEvent{}
