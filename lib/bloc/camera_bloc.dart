7439ffb41a6ec4cd1db2b2c395da691cf91d19ac
thekill022 <gibfathoni@gmail.com>
2026-04-17 10:36:13 +0700

    membuat method untuk mengambil gambar

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:cameraapp/bloc/camera_event.dart';
 import 'package:cameraapp/bloc/camera_state.dart';
@@ -50,6 +52,16 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
 
     await s.controller.setFlashMode(next);
     emit(s.copyWith(flashMode: next));
+  }
+
+  Future<void> _onTakePicture(
+      TakePicture event,
+      Emitter<CameraState> emit
+      ) async {
+    if(state is !CameraReady) return;
+    final s = state as CameraReady;
+    final file = await s.controller.takePicture();
+    event.onPictureTaken(File(file.path));
   }
 
 }
