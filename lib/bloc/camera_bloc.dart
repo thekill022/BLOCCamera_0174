13ea9a6790e5df056d5494691d47a21ec2f15367
thekill022 <gibfathoni@gmail.com>
2026-04-17 10:17:08 +0700

    membuat class utama untuk manage dengan bloc

---
+++ lib/bloc/camera_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:camera/camera.dart';
+import 'package:cameraapp/bloc/camera_event.dart';
+import 'package:cameraapp/bloc/camera_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CameraBloc extends Bloc<CameraEvent, CameraState> {
+
+}
