4deaa6129092eb00255ec48eac64e0a44f7748db
thekill022 <gibfathoni@gmail.com>
2026-04-19 20:41:30 +0700

    membuat method untuk mengambil folder directory dan save gambar

---
+++ lib/helpers/storage_helper.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:path/path.dart' as path;
+
+class StorageHelper {
+  static Future<String> _getFolderPath() async{
+    final dir = Directory('/storage/emulated/0/DCIM/FlutterNativeCamBloc');
+    if(!await dir.exists()) {
+      await dir.create(recursive: true);
+    }
+    return dir.path;
+  }
+
+  static Future<File> saveImage(File file, String prefix) async {
+    final dirPath = await _getFolderPath();
+    final fileName = '${prefix}_${DateTime.now().millisecondsSinceEpoch}${path.extension(file.path)}';
+    final savedPath = path.join(dirPath, fileName);
+    return await file.copy(savedPath);
+  }
+}
